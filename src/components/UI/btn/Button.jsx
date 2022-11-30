0bb70aaea4af625302cdcd5344510143e50ea89f
urtesala <urtesaladziute@gmail.com>
2022-11-30 11:15:20 +0300

    small btn toggle

--- src/components/UI/btn/Button.jsx
+++ src/components/UI/btn/Button.jsx
@@ -3,9 +3,16 @@ import css from './Button.module.css';
 function Button(props) {
   const btnSize = props.size;
   console.log('btnSize ===', btnSize);
+  // jei btnSize yra lg tai reikia prideti papildomai clase css.large
+
+  const largeBtn = btnSize === 'lg' ? css.large : '';
+  const smallBtn = btnSize === 'sm' ? css.small : '';
+
+  // jei gavim props secodary, tai prideam klase .secondary
+  // aprasyti klase .secondary - bacground color coral
 
   return (
-    <button className={`${css.btn} ${btnSize === 'lg' ? css.large : ''}`}>
+    <button className={`${css.btn} ${largeBtn} ${smallBtn}`}>
       {props.children}
     </button>
   );
