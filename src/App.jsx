676f5d79a1eab0f40bfc4e379f2fe0c39f3dace3
urtesala <urtesaladziute@gmail.com>
2022-11-30 08:43:13 +0300

    css bleed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Card from './components/card/Card'
 import './reset.css'
 
 function App() {
   return (
     <div className="App container">
       <h1>React</h1>
+      <Card/>
     </div>
   )
 }
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,16 @@
+import './Card.css';
+
+function Card() {
+  return (
+    <div className='card'>
+      <h2 className='title'>James Bond</h2>
+      <h3 className='subtitle'>Title: 007</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe,
+        temporibus. Dicta dolorem eos at mollitia doloribus modi minus
+        voluptate. Ipsam.
+      </p>
+    </div>
+  );
+}
+export default Card;
